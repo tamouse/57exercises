7adaf42faf7523a6a0f148ec5f996ac74d83a309
Tamara Temple <tamouse@gmail.com>
2016-12-24 07:58:41 -0600

    Convert to class, add tests

---
+++ 24_anagram_checker/test_anagram_checker.py
@@ -0,0 +1,11 @@
+
+import unittest
+from anagram_checker import AnagramChecker
+
+class AnagramCheckerTestCase(unittest.TestCase):
+
+    def test_two_anagrams(self):
+        word1='hello'
+        word2='o hell'
+        ac=AnagramChecker(word1, word2)
+        self.assertTrue(ac.check())
