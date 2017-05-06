3da840bec39c575111378ebeb3712923c3311b29
Tamara Temple <tamouse@gmail.com>
2017-05-06 11:36:34 -0500

    calc number of pizzas needed

--- ex08_pizza_party/__main__.py
+++ ex08_pizza_party/__main__.py
@@ -1,9 +1,30 @@
 from ex08_pizza_party.calc import *
 
-numppl = input("How many people? ")
-numpizzas = input("How many pizzas? ")
-numslices = input("How many slices per pizza? ")
+numppl = None
+numpizzas = None
+numslicesper = None
+numslices = None
 
-slicesPerPerson, slicesLeftOver = calc(numppl, numpizzas, numslices)
+numppl = input("How many people? ").strip()
+numpizzas = input("How many pizzas? (Press enter if you don't know) ").strip()
 
-print("\nNumber of slices per person: {}\nNumber of slices remaining: {}\n".format(slicesPerPerson, slicesLeftOver))
+if numpizzas == '':
+  numslicesper = input("Ok, how many slices per person, then? ").strip()
+  numpizzas = None
+
+numslices = input("How many slices per pizza? ").strip()
+
+try:
+  if numslicesper is not None:
+    numPizzasNeeded, slicesLeftOver = calcPizzas(numppl, numslicesper, numslices)
+    print("\nNumber of pizzas needed: {}\nNumber of slices remaining: {}\n".format(numPizzasNeeded, slicesLeftOver))
+
+  elif numpizzas is not None:
+    slicesPerPerson, slicesLeftOver = calcSlices(numppl, numpizzas, numslices)
+    print("\nNumber of slices per person: {}\nNumber of slices remaining: {}\n".format(slicesPerPerson, slicesLeftOver))
+
+  else:
+    raise Exception("I don't know what to do!")
+
+except Exception as e:
+  print("Woops:" , str(e))
--- ex08_pizza_party/calc.py
+++ ex08_pizza_party/calc.py
@@ -1,34 +1,72 @@
 
 
-def calc(numppl, numpizzas, numslices):
+def calcPizzas(numppl, numslicesper, numslices):
+  """
+  Given the number of people, the number of slices per person, and the number of slices per pizze,
+  return the number of pizzas needed to feed everyone, and the number of pieces left over.
+
+  >>> calcPizzas(8, 1, 8)
+  (1, 0)
+
+  >>> calcPizzas(22, 3, 8)
+  (8, 2)
+
+  >>> calcPizzas("A", 1, 8)
+  Traceback (most recent call last):
+      ...
+  ValueError: Number of People must be an integer greater than zero
+
+  >>> calcPizzas(8, "a", 8)
+  Traceback (most recent call last):
+      ...
+  ValueError: Number of Slices per Person must be an integer greater than zero
+
+  >>> calcPizzas(8, 1, "a")
+  Traceback (most recent call last):
+      ...
+  ValueError: Number of Slices per Pizza must be an integer greater than zero
+
+  """
+
+  numppl = checkInt(numppl, "Number of People")
+  numslicesper = checkInt(numslicesper, "Number of Slices per Person")
+  numslices = checkInt(numslices, "Number of Slices per Pizza")
+
+  slicesNeeded = numppl * numslicesper
+  pizzasNeeded = slicesNeeded // numslices
+  slicesLeftOver = slicesNeeded % numslices
+  return pizzasNeeded, slicesLeftOver
+
+
+def calcSlices(numppl, numpizzas, numslices):
   """
   Given the number of people, pizzas, and slices per pizza, return
   the number of slices each person gets, and the number of leftover
   slices.
 
-  >>> calc(8, 2, 8)
+  >>> calcSlices(8, 2, 8)
   (2, 0)
 
-  >>> calc(7, 1, 8)
+  >>> calcSlices(7, 1, 8)
   (1, 1)
 
-  >>> calc("A", 1, 8)
+  >>> calcSlices("A", 1, 8)
   Traceback (most recent call last):
       ...
   ValueError: Number of People must be an integer greater than zero
 
-  >>> calc(8, "A", 8)
+  >>> calcSlices(8, "A", 8)
   Traceback (most recent call last):
       ...
   ValueError: Number of Pizzas must be an integer greater than zero
 
 
-  >>> calc(8, 1, "A")
+  >>> calcSlices(8, 1, "A")
   Traceback (most recent call last):
       ...
   ValueError: Number of Slices must be an integer greater than zero
 
-  >>> calc(22, 1, 8)
+  >>> calcSlices(22, 1, 8)
   Traceback (most recent call last):
       ...
   ValueError: Woopss!! Not enough pizzas to feed everyone!!
