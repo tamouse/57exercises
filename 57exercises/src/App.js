3d3206ef27afe70effb6068281e3f8d88a241e90
Tamara Temple <tamouse@gmail.com>
2017-06-02 21:05:56 -0500

    First touch, retitle the page.

--- 57exercises/src/App.js
+++ 57exercises/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Brian P. Hogan's 57 Exercises for Programmers implemented in React.JS</h2>
+          <h3><a href="https://github.com/tamouse" target="_blank" title="Github Repo" rel="noopener noreferrer">Implemented by Tamara Temple </a></h3>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
